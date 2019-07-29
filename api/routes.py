b788f4fd5a7453ecff28a1d5df988ac3dc49c4aa
Gabriel Riera <gabriel.riera@uib.es>
2019-07-29 14:07:48 +0200

    formatting

--- api/routes.py
+++ api/routes.py
@@ -6,12 +6,12 @@ routes_ping = [
 
 ]
 stringdb_routes = [
-   ['POST', '/db/stringdb/items/species/select',  stringdb.select_species],
-   ['POST', '/db/stringdb/items/proteins/select', stringdb.select_proteins],
-   ['POST', '/db/stringdb/network/edges/select',  stringdb.select_network_edges],
-   ['GET', '/db/stringdb/network/edges/weighted', stringdb.get_weighted_network_edges],
-   ['POST', '/db/stringdb/bitscore/select',       stringdb.select_bitscores],
-   ['POST', '/db/stringdb/go/annotations/select', stringdb.select_go_annotations],
+   ['POST', '/db/stringdb/items/species/select',   stringdb.select_species],
+   ['POST', '/db/stringdb/items/proteins/select',  stringdb.select_proteins],
+   ['POST', '/db/stringdb/network/edges/select',   stringdb.select_network_edges],
+   ['GET',  '/db/stringdb/network/edges/weighted', stringdb.get_weighted_network_edges],
+   ['POST', '/db/stringdb/bitscore/select',        stringdb.select_bitscores],
+   ['POST', '/db/stringdb/go/annotations/select',  stringdb.select_go_annotations],
 
 ]
 
